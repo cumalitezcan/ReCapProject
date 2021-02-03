16633f60f8381772d33ca5aa540e0b1f9c716b52
Cumali Tezcan <34379535+cumalitezcan@users.noreply.github.com>
2021-02-03 20:35:05 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,58 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            Console.WriteLine("---Araba Ekleme İşlemleri---");
+            carManager.Add(new Car { Id = 4, BrandId = 6, ColorId = 7, ModelYear = 1999, DailyPrice = 14999, Description = "Renault-Broadway" });
+            Console.WriteLine();
+
+            Console.WriteLine("---Mevcut Arabalar---");
+            foreach (var item in carManager.GetAll())
+            {
+                Console.WriteLine(item.Description);
+                
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("---Özel Araç Bilgisi---");
+            foreach (var item in carManager.GetById(2))
+            {
+                Console.WriteLine("{0} id numaralı araç: {1} ",item.Id,item.Description);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("---Araç Güncelleme İşlemi---");
+            carManager.Update(new Car { Id = 4, BrandId = 4, ColorId = 4, ModelYear = 1997, DailyPrice = 12500, Description = "Renault-Spring" });
+
+            Console.WriteLine("---Mevcut Araçlar---");
+            foreach (var item in carManager.GetAll())
+            {
+                Console.WriteLine(item.Description);
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("Araç Silme İşlemi");
+            carManager.Delete(new Car { Id = 4 });
+
+            Console.WriteLine("---Mevcut Araçlar---");
+            foreach (var item in carManager.GetAll())
+            {
+                Console.WriteLine(item.Description);
+
+            }
+            
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,64 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace DataAccess.Concrete.InMemory
+{
+   public class InMemoryCarDal:ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car(){ Id=1,BrandId=3,ColorId=5,ModelYear=1990,DailyPrice=35000,Description="Fiat-Palio"},
+                new Car(){ Id=2,BrandId=4,ColorId=6,ModelYear=1995,DailyPrice=45000,Description="Hundai Accent"},
+                new Car(){ Id=3,BrandId=5,ColorId=6,ModelYear=1999,DailyPrice=55000,Description="Opel Astra"}
+            };
+
+            
+        }
+
+        public void Add(Car car)
+        {
+            
+            _cars.Add(car);
+            Console.WriteLine(car.Description+" isimli araba eklendi");
+        }
+
+        public void Delete(Car car)
+        {
+             
+            Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int id)
+        {
+            var idGetir = from c in _cars
+                          where c.Id == id
+                          select c;
+
+            return idGetir.ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+           
+        }
+    }
+}
+
