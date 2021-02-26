3e9148511206da052cb6c9d4cf86a8842375d109
Cumali Tezcan <34379535+cumalitezcan@users.noreply.github.com>
2021-02-27 00:44:06 +0300

    addedFluentValidation

---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+   public class CarValidator:AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c => c.Descriptions).NotEmpty(); ;
+            RuleFor(c=>c.Descriptions).MinimumLength(2);
+            RuleFor(c => c.ModelYear).NotEmpty();
+            RuleFor(c => c.DailyPrice).GreaterThanOrEqualTo(50).When(c => c.BrandId == 1);
+           // RuleFor(c=>c.Descriptions).Must(StartWithA); //Description A ile başlamalı
+        }
+    }
+}
---
+++ Core/CrossCuttingConcerns/Validation/ValidationTool.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.CrossCuttingConcerns.Validation
+{
+   public static class ValidationTool
+    {
+        //Car Validator'ı kullanmamız için...
+        public static void Validate(IValidator validator,object entity)
+        {
+            
+            //Context, ilgili bir thread'i anlatır.
+            var context = new ValidationContext<object>(entity);
+
+            //RuleFor kurallarımız için ilgili contex'i doğrula.
+            var result = validator.Validate(context);
+            if (!result.IsValid)
+            {
+                throw new ValidationException(result.Errors);
+            }
+        }
+
+        
+    }
+}
