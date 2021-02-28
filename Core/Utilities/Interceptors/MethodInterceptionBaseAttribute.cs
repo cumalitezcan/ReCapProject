35fa100f7d228563e2709a7eccc8359c7dea3518
Cumali Tezcan <34379535+cumalitezcan@users.noreply.github.com>
2021-02-28 20:19:07 +0300

    addedAOP

---
+++ Core/Utilities/Interceptors/MethodInterceptionBaseAttribute.cs
@@ -0,0 +1,21 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Interceptors
+{
+    //Bu Attribute classlara veya methodlara eklenebilir.
+    //Birden fazla eklenebilir ve inherit edilen bir noktada da çalışsın.
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    public abstract class MethodInterceptionBaseAttribute : Attribute, IInterceptor
+    {
+        public int Priority { get; set; }
+
+        public virtual void Intercept(IInvocation invocation)
+        {
+
+        }
+    }
+
+}
